8814076867c6c6a8ea66844220f4d4797daa33b8
Jamie Smith <jamiesmith357@gmail.com>
2020-11-27 12:10:28 -0500

    Fixing issues with lines by using new Date().

--- app/index.js
+++ app/index.js
@@ -12,13 +12,13 @@ import { battery, charger } from "power";
 clock.granularity = 'seconds';
 
 const timeLabel = document.getElementById("timeLabel");
-let secLabel = document.getElementById("secLabel");
-let dateLabel = document.getElementById("dateLabel");
-let dayLabel = document.getElementById("dayLabel");
-let caloriesLabel = document.getElementById("caloriesLabel");
-let stepsLabel = document.getElementById("stepsLabel");
-let heartRateLabel = document.getElementById("heartRateLabel");
-let batteryLabel = document.getElementById("batteryLabel");
+const secLabel = document.getElementById("secLabel");
+const dateLabel = document.getElementById("dateLabel");
+const dayLabel = document.getElementById("dayLabel");
+const caloriesLabel = document.getElementById("caloriesLabel");
+const stepsLabel = document.getElementById("stepsLabel");
+const heartRateLabel = document.getElementById("heartRateLabel");
+const batteryLabel = document.getElementById("batteryLabel");
 
 let stepsThisPoint = 0;
 let stepsOffset = today.adjusted.steps;
@@ -29,56 +29,34 @@ let initialPoint = true;
 let maxLineHeight = 20;
 let maxSteps = 100;
 let maxCalories = 50;
-let hours;
-let mins;
+// let hours;
+// let mins;
 let initialize = true;
 let updatesRunning = false;
 const dotCount = 96;
 const interval = (Math.PI * 2) / dotCount;
 const timePointInts = [0, 15, 30, 45];
-let timePoints = {
-    0: { 0: { point: 72, visible: false }, 15: { point: 73, visible: false }, 30: { point: 74, visible: false }, 45: { point: 75, visible: false } },
-    1: { 0: { point: 76, visible: false }, 15: { point: 77, visible: false }, 30: { point: 78, visible: false }, 45: { point: 79, visible: false } },
-    2: { 0: { point: 80, visible: false }, 15: { point: 81, visible: false }, 30: { point: 82, visible: false }, 45: { point: 83, visible: false } },
-    3: { 0: { point: 84, visible: false }, 15: { point: 85, visible: false }, 30: { point: 86, visible: false }, 45: { point: 87, visible: false } },
-    4: { 0: { point: 88, visible: false }, 15: { point: 89, visible: false }, 30: { point: 90, visible: false }, 45: { point: 91, visible: false } },
-    5: { 0: { point: 92, visible: false }, 15: { point: 93, visible: false }, 30: { point: 94, visible: false }, 45: { point: 95, visible: false } },
-    6: { 0: { point: 96, visible: false }, 15: { point: 1, visible: false }, 30: { point: 2, visible: false }, 45: { point: 3, visible: false } },
-    7: { 0: { point: 4, visible: false }, 15: { point: 5, visible: false }, 30: { point: 6, visible: false }, 45: { point: 7, visible: false } },
-    8: { 0: { point: 8, visible: false }, 15: { point: 9, visible: false }, 30: { point: 10, visible: false }, 45: { point: 11, visible: false } },
-    9: { 0: { point: 12, visible: false }, 15: { point: 13, visible: false }, 30: { point: 14, visible: false }, 45: { point: 15, visible: false } },
-    10: { 0: { point: 16, visible: false }, 15: { point: 17, visible: false }, 30: { point: 18, visible: false }, 45: { point: 19, visible: false } },
-    11: { 0: { point: 20, visible: false }, 15: { point: 21, visible: false }, 30: { point: 22, visible: false }, 45: { point: 23, visible: false } },
-    12: { 0: { point: 24, visible: false }, 15: { point: 25, visible: false }, 30: { point: 26, visible: false }, 45: { point: 27, visible: false } },
-    13: { 0: { point: 28, visible: false }, 15: { point: 29, visible: false }, 30: { point: 30, visible: false }, 45: { point: 31, visible: false } },
-    14: { 0: { point: 32, visible: false }, 15: { point: 33, visible: false }, 30: { point: 34, visible: false }, 45: { point: 35, visible: false } },
-    15: { 0: { point: 36, visible: false }, 15: { point: 37, visible: false }, 30: { point: 38, visible: false }, 45: { point: 39, visible: false } },
-    16: { 0: { point: 40, visible: false }, 15: { point: 41, visible: false }, 30: { point: 42, visible: false }, 45: { point: 43, visible: false } },
-    17: { 0: { point: 44, visible: false }, 15: { point: 45, visible: false }, 30: { point: 46, visible: false }, 45: { point: 47, visible: false } },
-    18: { 0: { point: 48, visible: false }, 15: { point: 49, visible: false }, 30: { point: 50, visible: false }, 45: { point: 51, visible: false } },
-    19: { 0: { point: 52, visible: false }, 15: { point: 53, visible: false }, 30: { point: 54, visible: false }, 45: { point: 55, visible: false } },
-    20: { 0: { point: 56, visible: false }, 15: { point: 57, visible: false }, 30: { point: 58, visible: false }, 45: { point: 59, visible: false } },
-    21: { 0: { point: 60, visible: false }, 15: { point: 61, visible: false }, 30: { point: 62, visible: false }, 45: { point: 63, visible: false } },
-    22: { 0: { point: 64, visible: false }, 15: { point: 65, visible: false }, 30: { point: 66, visible: false }, 45: { point: 67, visible: false } },
-    23: { 0: { point: 68, visible: false }, 15: { point: 69, visible: false }, 30: { point: 70, visible: false }, 45: { point: 71, visible: false } },
-}
+let date = new Date();
+let hours = date.getHours();
+let mins = date.getMinutes();
+let checkIfCorrectInterval;
 
 clock.ontick = (evt) => {
   let today_dt = evt.date;
-  hours = today_dt.getHours();
-  mins = today_dt.getMinutes();
+  let clockHours = today_dt.getHours();
+  let clockMins = today_dt.getMinutes();
   let secs = today_dt.getSeconds();
   let day = today_dt.getDate();
   let hoursFmted;
 
   if (preferences.clockDisplay === "12h") {
     // 12h format
-    hoursFmted = utils.monoDigits(utils.zeroPad(hours % 12 || 12));
+    hoursFmted = utils.monoDigits(utils.zeroPad(clockHours % 12 || 12));
   } else {
     // 24h format
-    hoursFmted = utils.monoDigits(utils.zeroPad(hours));
+    hoursFmted = utils.monoDigits(utils.zeroPad(clockHours));
   };
-  let minsFmted = utils.monoDigits(utils.zeroPad(mins));
+  let minsFmted = utils.monoDigits(utils.zeroPad(clockMins));
   let secsFmted = utils.monoDigits(utils.zeroPad(secs));
   
   timeLabel.text = `${hoursFmted}:${minsFmted}`;  
@@ -89,48 +67,53 @@ clock.ontick = (evt) => {
   dayLabel.text = `${weekday}`;
   dateLabel.text = `${month} ${day}`;
 
-  if (initialize) {
-    initialize = false;
-    updateActivityLine();
-    checkPreviousLines();
-  }
-  
-  if (!updatesRunning) {
-    // Hack to wake system
-    if (display.off) {
-      display.poke();
-      display.on = false;
-    }
+  // Add calories label
+  let calories = (today.adjusted.calories || 0).toLocaleString();
+  caloriesLabel.text = calories;
 
+   // Add steps label
+  let steps = (today.adjusted.steps || 0).toLocaleString();
+  stepsLabel.text = steps;
+
+  // Battery label
+  let batteryLevel = Math.floor(battery.chargeLevel);
+  let batterLevelFmted = '';
+  if (batteryLevel > 0) {
+      batterLevelFmted = `${utils.monoDigits(batteryLevel, false)}%`;
+  }
+  batteryLabel.text = batterLevelFmted;
+  
+  // For testing - update every second
+  // updateActivityLine();
+}
+
+if (initialize) {
+  initialize = false;
+  updateActivityLine();
+  checkPreviousLines();
+}
+
+if (!updatesRunning) {
+  checkIfCorrectInterval = setInterval(function() {
+    // Every minute
+    date = new Date();
+    hours = date.getHours();
+    mins = date.getMinutes();
+
+    // Hack to wake system for step count
+    display.poke();
+    display.on = false;
+    
     if (mins === 0 || mins === 15 || mins === 30 || mins === 45) {
       // Start automated line drawing we're on a 15 minute interval
       updatesRunning = true;
       updateActivityLines();
       drawActivityLines();
     }
-  }
-  
-    // Add calories label
-    let calories = (today.adjusted.calories || 0).toLocaleString();
-    caloriesLabel.text = calories;
-  
-     // Add steps label
-    let steps = (today.adjusted.steps || 0).toLocaleString();
-    stepsLabel.text = steps;
-  
-    // Battery label
-    let batteryLevel = Math.floor(battery.chargeLevel);
-    let batterLevelFmted = '';
-    if (batteryLevel > 0) {
-        batterLevelFmted = `${utils.monoDigits(batteryLevel, false)}%`;
-    }
-    batteryLabel.text = batterLevelFmted;
-  
-  // For testing - update every second
-  // updateActivityLine();
+  }, 60000);
 }
 
-// For testing - adding all points
+// For testing - adds all points
 // for (let i = 1; i <= 96; i++) {
 //   let height = 20;
 //   // let height = Math.floor(Math.random() * 20);
@@ -180,14 +163,14 @@ function resetPoints() {
 
   for (var i = 0; i <= 23; i++) {
     for (var j = 0; j < timePointInts.length; j++) {
-      timePoints[i][j]['visible'] = false;
+      utils.timePoints[i][j]['visible'] = false;
     }
   }
 }
 
 function getCurrentPoint() {
   if (mins === 0 || mins === 15 || mins === 30 || mins === 45) {
-    return timePoints[hours][mins];
+    return utils.timePoints[hours][mins];
   }
 
   let timePos = 0;
@@ -199,7 +182,7 @@ function getCurrentPoint() {
     timePos = 45;
   }
   
-  return timePoints[hours][timePos];
+  return utils.timePoints[hours][timePos];
 }
 
 function calculateLineHeight(total, type) {
@@ -222,16 +205,13 @@ function calculateLineHeight(total, type) {
     }
   }
   
-  console.log(`returning lineHeight ${lineHeight} for ${type}`)
   return lineHeight;
 }
 
 function addActivityLine(point = null) {
-  console.log('addActivityLine running');
   if (!point) {
     point = getCurrentPoint();
   }
-  console.log(`currentPoint: ${point['point']}`);
   setPoint(point['point'], 0, "calories");
   setPoint(point['point'], 0, "step");
 
@@ -239,29 +219,24 @@ function addActivityLine(point = null) {
 }
 
 function updateActivityLine() {
-  console.log('updateActivityLine running');
   const currentLine = getCurrentPoint();
   const currentPoint = currentLine['point'];
-  console.log(`currentPoint: ${currentPoint}`);
   let lineHeight;
 
   // Calculate calories since the hour
   caloriesThisPoint = today.adjusted.calories - caloriesOffset || 0;
   lineHeight = calculateLineHeight(caloriesThisPoint, "calories");
-  console.log(`setting point: ${currentPoint}, ${lineHeight}, calories`);
   setPoint(currentPoint, lineHeight, "calories");
   
   // Calculate steps since the hour
   stepsThisPoint = today.adjusted.steps - stepsOffset || 0;
   lineHeight = calculateLineHeight(stepsThisPoint, "step");
-  console.log(`setting point: ${currentPoint}, ${lineHeight}, step`);
   setPoint(currentPoint, lineHeight, "step");
 
   currentLine['visible'] = true;
 }
 
 async function setPreviousActivityLine(total, type) {
-  console.log(`setPreviousActivityLine running with: ${total}, ${type}`)
   const currentLine = getCurrentPoint();
   const currentPoint = currentLine['point'];
   let lineHeight;
@@ -275,12 +250,10 @@ async function setPreviousActivityLine(total, type) {
   if (type === "calories") {
     caloriesThisPoint = today.adjusted.calories - total || 0;
     lineHeight = calculateLineHeight(caloriesThisPoint, "calories");
-    console.log(`forpreviousline, setting: point: ${currentPoint}, ${lineHeight}, calories`)
     setPoint(currentPoint, lineHeight, "calories");
   } else if (type === "step") {
     stepsThisPoint = today.adjusted.steps - total || 0;
     lineHeight = calculateLineHeight(stepsThisPoint, "step");
-    console.log(`forpreviousline, setting: point: ${currentPoint}, ${lineHeight}, step`)
     setPoint(currentPoint, lineHeight, "step");
   }
 
@@ -290,29 +263,28 @@ async function setPreviousActivityLine(total, type) {
 }
 
 function checkPreviousLines() {
-  console.log('checkPreviousLines running');
   // Check mins from this hour for missing lines
 
   // If current time is < :15, no need to check previous minutes
   if (mins > 15) {
     // Check :00 line
     if (mins > 15) {
-      if (!timePoints[hours][0]['visible']) {
-        addActivityLine(timePoints[hours][0]);
+      if (!utils.timePoints[hours][0]['visible']) {
+        addActivityLine(utils.timePoints[hours][0]);
       }
     }
 
     // Check :15 line
     if (mins >= 30) {
-      if (!timePoints[hours][15]['visible']) {
-        addActivityLine(timePoints[hours][15]);
+      if (!utils.timePoints[hours][15]['visible']) {
+        addActivityLine(utils.timePoints[hours][15]);
       }
     }
 
    // Check :30 line
     if (mins >= 45) {
-      if (!timePoints[hours][30]['visible']) {
-        addActivityLine(timePoints[hours][30]);
+      if (!utils.timePoints[hours][30]['visible']) {
+        addActivityLine(utils.timePoints[hours][30]);
       }
     }
   }
@@ -322,9 +294,9 @@ function checkPreviousLines() {
     for (var i = hours - 1; i >= 0; i--) {
       for (var j = 0; j < timePointInts.length; j++) {
         let minutes = timePointInts[j];
-        if (!timePoints[i][minutes]['visible']) {
+        if (!utils.timePoints[i][minutes]['visible']) {
           // Point is not visible, add it
-          addActivityLine(timePoints[i][minutes]);
+          addActivityLine(utils.timePoints[i][minutes]);
         }
       }
     }
@@ -332,11 +304,13 @@ function checkPreviousLines() {
 }
 
 async function updateActivityLines() {
+  date = new Date();
+  hours = date.getHours();
+  mins = date.getMinutes();
   if (hours === 0 && mins === 0) {
     // Current point is midnight, reset points
     resetPoints();
   } else {
-    console.log('not midnight, setting previous counts!');
     // Set final count for previous line
     await setPreviousActivityLine(caloriesOffset, "calorie");
     await setPreviousActivityLine(stepsOffset, "step");
@@ -344,12 +318,10 @@ async function updateActivityLines() {
     checkPreviousLines();
   }
 
-  console.log('resetting steps');
   // Reset steps
   stepsThisPoint = 0;
   stepsOffset = today.adjusted.steps;
 
-  console.log('resetting calories');
   // Reset calories
   caloriesThisPoint = 0;
   caloriesOffset = today.adjusted.calories;
@@ -358,8 +330,8 @@ async function updateActivityLines() {
 }
 
 function drawActivityLines() {
+  clearInterval(checkIfCorrectInterval);
   setInterval(function() {
-    console.log('setInterval running');
     // Every 15 mins
 
     // Hack to wake system for step count
@@ -371,6 +343,9 @@ function drawActivityLines() {
 }
 
 display.onchange = function() {
+  date = new Date();
+  hours = date.getHours();
+  mins = date.getMinutes();
   if (display.on && mins !== 0 && mins !== 15 && mins !== 30 && mins !== 45) {
     // Screen is on - update activity line
     updateActivityLine();
--- resources/utils.js
+++ resources/utils.js
@@ -35,6 +35,33 @@ export function hex2a(hex) {
   return str.toString();
 }
 
+export const timePoints = {
+    0: { 0: { point: 72, visible: false }, 15: { point: 73, visible: false }, 30: { point: 74, visible: false }, 45: { point: 75, visible: false } },
+    1: { 0: { point: 76, visible: false }, 15: { point: 77, visible: false }, 30: { point: 78, visible: false }, 45: { point: 79, visible: false } },
+    2: { 0: { point: 80, visible: false }, 15: { point: 81, visible: false }, 30: { point: 82, visible: false }, 45: { point: 83, visible: false } },
+    3: { 0: { point: 84, visible: false }, 15: { point: 85, visible: false }, 30: { point: 86, visible: false }, 45: { point: 87, visible: false } },
+    4: { 0: { point: 88, visible: false }, 15: { point: 89, visible: false }, 30: { point: 90, visible: false }, 45: { point: 91, visible: false } },
+    5: { 0: { point: 92, visible: false }, 15: { point: 93, visible: false }, 30: { point: 94, visible: false }, 45: { point: 95, visible: false } },
+    6: { 0: { point: 96, visible: false }, 15: { point: 1, visible: false }, 30: { point: 2, visible: false }, 45: { point: 3, visible: false } },
+    7: { 0: { point: 4, visible: false }, 15: { point: 5, visible: false }, 30: { point: 6, visible: false }, 45: { point: 7, visible: false } },
+    8: { 0: { point: 8, visible: false }, 15: { point: 9, visible: false }, 30: { point: 10, visible: false }, 45: { point: 11, visible: false } },
+    9: { 0: { point: 12, visible: false }, 15: { point: 13, visible: false }, 30: { point: 14, visible: false }, 45: { point: 15, visible: false } },
+    10: { 0: { point: 16, visible: false }, 15: { point: 17, visible: false }, 30: { point: 18, visible: false }, 45: { point: 19, visible: false } },
+    11: { 0: { point: 20, visible: false }, 15: { point: 21, visible: false }, 30: { point: 22, visible: false }, 45: { point: 23, visible: false } },
+    12: { 0: { point: 24, visible: false }, 15: { point: 25, visible: false }, 30: { point: 26, visible: false }, 45: { point: 27, visible: false } },
+    13: { 0: { point: 28, visible: false }, 15: { point: 29, visible: false }, 30: { point: 30, visible: false }, 45: { point: 31, visible: false } },
+    14: { 0: { point: 32, visible: false }, 15: { point: 33, visible: false }, 30: { point: 34, visible: false }, 45: { point: 35, visible: false } },
+    15: { 0: { point: 36, visible: false }, 15: { point: 37, visible: false }, 30: { point: 38, visible: false }, 45: { point: 39, visible: false } },
+    16: { 0: { point: 40, visible: false }, 15: { point: 41, visible: false }, 30: { point: 42, visible: false }, 45: { point: 43, visible: false } },
+    17: { 0: { point: 44, visible: false }, 15: { point: 45, visible: false }, 30: { point: 46, visible: false }, 45: { point: 47, visible: false } },
+    18: { 0: { point: 48, visible: false }, 15: { point: 49, visible: false }, 30: { point: 50, visible: false }, 45: { point: 51, visible: false } },
+    19: { 0: { point: 52, visible: false }, 15: { point: 53, visible: false }, 30: { point: 54, visible: false }, 45: { point: 55, visible: false } },
+    20: { 0: { point: 56, visible: false }, 15: { point: 57, visible: false }, 30: { point: 58, visible: false }, 45: { point: 59, visible: false } },
+    21: { 0: { point: 60, visible: false }, 15: { point: 61, visible: false }, 30: { point: 62, visible: false }, 45: { point: 63, visible: false } },
+    22: { 0: { point: 64, visible: false }, 15: { point: 65, visible: false }, 30: { point: 66, visible: false }, 45: { point: 67, visible: false } },
+    23: { 0: { point: 68, visible: false }, 15: { point: 69, visible: false }, 30: { point: 70, visible: false }, 45: { point: 71, visible: false } },
+}
+
 // Match time to point
 export function timeToPoint() {
  return {
