f403c5a2fb1af3368983cf39878f445a15dee70b
Jamie Smith <jamiesmith357@gmail.com>
2021-01-23 13:26:22 -0500

    Adding opacity back in for reset.

--- app/index.js
+++ app/index.js
@@ -245,7 +245,7 @@ function resetPoints() {
       for (let c = 0; c < coordinates.length; c++) {
         pointElements[p][c] = 149;
       }
-      // pointElements[p]['style']['opacity'] = 1;
+      pointElements[p]['style']['opacity'] = 1;
     }
   }
 
